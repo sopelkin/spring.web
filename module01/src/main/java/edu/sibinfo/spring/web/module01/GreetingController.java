610eae613ad064ea026aae05d71eeb7f09aeebac
Kirill Karmakulov <karmakulov@improveitgroup.com>
2017-06-03 22:24:10 +0700

    Module 01 - Dynamic content, @RequestParam

--- module01/src/main/java/edu/sibinfo/spring/web/module01/GreetingController.java
+++ module01/src/main/java/edu/sibinfo/spring/web/module01/GreetingController.java
@@ -5,12 +5,14 @@ import java.time.LocalDateTime;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
     @RequestMapping("/greeting")
-    public String greeting(Model model) {
-    	model.addAttribute("timeStamp", LocalDateTime.now().toString());
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+    	model.addAttribute("timeStamp", LocalDateTime.now());
+    	model.addAttribute("name", name);
         return "greeting";
     }
 }
