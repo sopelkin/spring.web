787e48f3ef8d4114b591fe46f11621e589dfcba4
Kirill Karmakulov <karmakulov@improveitgroup.com>
2017-06-03 22:08:14 +0700

    Module 01 - Web application, static content

---
+++ module01/src/main/java/edu/sibinfo/spring/web/module01/Module01Application.java
@@ -0,0 +1,12 @@
+package edu.sibinfo.spring.web.module01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module01Application.class, args);
+	}
+}
