fade872b434da85119b178135dfa38b628a2b9a7
Kirill Karmakulov <karmakulov@improveitgroup.com>
2017-06-13 03:07:03 +0700

    Module 04 - XML API for ClientService

--- module04/src/main/java/edu/sibinfo/spring/web/module04/dto/ClientDTO.java
+++ module04/src/main/java/edu/sibinfo/spring/web/module04/dto/ClientDTO.java
@@ -1,11 +1,16 @@
 package edu.sibinfo.spring.web.module04.dto;
 
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
 import lombok.Data;
 
+@XmlRootElement(name="client")
 @Data
 public class ClientDTO {
 	private Long id;
 	private String lastName;
 	private String name;
-	private Iterable<PhoneDTO> phones;
+	private List<PhoneDTO> phones;
 }
--- module04/src/main/java/edu/sibinfo/spring/web/module04/dto/ClientRegistrationDTO.java
+++ module04/src/main/java/edu/sibinfo/spring/web/module04/dto/ClientRegistrationDTO.java
@@ -1,7 +1,10 @@
 package edu.sibinfo.spring.web.module04.dto;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
 import lombok.Data;
 
+@XmlRootElement(name = "register")
 @Data
 public class ClientRegistrationDTO {
 	private String familyName;
