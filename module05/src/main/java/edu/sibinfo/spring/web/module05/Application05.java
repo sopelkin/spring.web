bd7c178b5774ec6f9e4cc4e3e19e98096ed6786d
Kirill Karmakulov <kkarmakul@gmail.com>
2017-06-21 09:00:26 +0700

    Module 05 - Uber-WAR for Tomcat deployment

--- module05/src/main/java/edu/sibinfo/spring/web/module05/Application05.java
+++ module05/src/main/java/edu/sibinfo/spring/web/module05/Application05.java
@@ -2,11 +2,18 @@ package edu.sibinfo.spring.web.module05;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
-public class Application05 {
+public class Application05 extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application05.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application05.class, args);
