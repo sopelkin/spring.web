8cff71f644ed2f9273c444a898298dce856c718b
teacher <teacher@TEACHER.samotlor.nips.ru>
2017-06-28 09:57:18 +0700

    - database props, app annotation, pom fix

--- module05/src/main/java/edu/sibinfo/spring/web/module05/Application05.java
+++ module05/src/main/java/edu/sibinfo/spring/web/module05/Application05.java
@@ -3,12 +3,13 @@ package edu.sibinfo.spring.web.module05;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
 @EnableTransactionManagement
 public class Application05 extends SpringBootServletInitializer {
 
