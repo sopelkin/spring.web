45e81f3a5f2f88aa832e83ed88166b997a9ffc0b
sbondarev <bondarev@improveitgroup.com>
2017-07-12 16:54:23 +0700

    -fix transactional problem in UserDetailsService

--- module05/src/main/java/edu/sibinfo/spring/web/module05/service/impl/UserDetailsServiceImpl.java
+++ module05/src/main/java/edu/sibinfo/spring/web/module05/service/impl/UserDetailsServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -26,7 +25,6 @@ public class UserDetailsServiceImpl implements UserDetailsService {
   }
 
   @Override
-  @Transactional(readOnly = true)
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
     User user;
     try {
