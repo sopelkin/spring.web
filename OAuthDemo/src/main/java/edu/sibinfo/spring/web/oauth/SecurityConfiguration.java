118cabf85665361081ba839342ed09ca5b1f118b
sbondarev <bondarev@improveitgroup.com>
2017-07-18 01:06:58 +0700

    -explicit configuration for client

--- OAuthDemo/src/main/java/edu/sibinfo/spring/web/oauth/SecurityConfiguration.java
+++ OAuthDemo/src/main/java/edu/sibinfo/spring/web/oauth/SecurityConfiguration.java
@@ -1,14 +1,32 @@
 package edu.sibinfo.spring.web.oauth;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+import javax.servlet.Filter;
+
 @Configuration
-@EnableOAuth2Sso
+@EnableOAuth2Client
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private OAuth2ClientContext oAuth2ClientContext;
 
     @SuppressWarnings("ProhibitedExceptionDeclared")
     @Override
@@ -20,11 +38,50 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/", "/login**", "/webjars/**").permitAll()
             .anyRequest().authenticated()
         .and()
+            .exceptionHandling()
+            .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/"/*"/login/github"*/))
+        .and()
             .logout()
             .logoutSuccessUrl("/").permitAll()
         .and()
             .csrf()
-            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+        .and()
+            .addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
 		// @formatter:on
+    }
+
+    @Bean
+    public FilterRegistrationBean oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(filter);
+        registration.setOrder(-100);
+        return registration;
+    }
+
+    @Bean
+    @ConfigurationProperties("github.client")
+    public AuthorizationCodeResourceDetails resourceDetails() {
+        return new AuthorizationCodeResourceDetails();
+    }
+
+    @Bean
+    @ConfigurationProperties("github.resource")
+    public ResourceServerProperties resource() {
+        return new ResourceServerProperties();
+    }
+
+    private Filter ssoFilter() {
+        OAuth2ClientAuthenticationProcessingFilter filter =
+                new OAuth2ClientAuthenticationProcessingFilter("/login/github");
+        OAuth2RestTemplate template = new OAuth2RestTemplate(resourceDetails(), oAuth2ClientContext);
+        filter.setRestTemplate(template);
+        ResourceServerProperties resource = resource();
+        UserInfoTokenServices tokenServices = new UserInfoTokenServices(resource.getUserInfoUri(),
+                resource.getClientId());
+        tokenServices.setRestTemplate(template);
+        filter.setTokenServices(
+                new UserInfoTokenServices(resource.getUserInfoUri(), resource.getClientId()));
+        return filter;
     }
 }
