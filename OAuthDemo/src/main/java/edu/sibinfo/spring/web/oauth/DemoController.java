4d01474da99d83ba0bcba921601151533e58c120
sbondarev <bondarev@improveitgroup.com>
2017-07-18 02:42:43 +0700

    -added proxy OAuth server )

--- OAuthDemo/src/main/java/edu/sibinfo/spring/web/oauth/DemoController.java
+++ OAuthDemo/src/main/java/edu/sibinfo/spring/web/oauth/DemoController.java
@@ -4,12 +4,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @RestController
 public class DemoController {
 
-    @RequestMapping("/user")
-    public Principal user(Principal principal) {
-        return principal;
+    @RequestMapping({"/me", "/user"})
+    public Map<String, String> user(Principal principal) {
+        Map<String, String> map = new LinkedHashMap<>();
+        map.put("name", principal.getName());
+        return map;
     }
 }
--- OAuthDemo/src/main/java/edu/sibinfo/spring/web/oauth/SecurityConfiguration.java
+++ OAuthDemo/src/main/java/edu/sibinfo/spring/web/oauth/SecurityConfiguration.java
@@ -1,12 +1,15 @@
 package edu.sibinfo.spring.web.oauth;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
@@ -14,7 +17,10 @@ import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
@@ -22,7 +28,9 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import javax.servlet.Filter;
 
 @Configuration
+@EnableAuthorizationServer
 @EnableOAuth2Client
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -35,7 +43,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		http
             .antMatcher("/**")
             .authorizeRequests()
-            .antMatchers("/", "/login**", "/webjars/**").permitAll()
+            .antMatchers("/", "/login**").permitAll()
             .anyRequest().authenticated()
         .and()
             .exceptionHandling()
@@ -60,28 +68,52 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    @ConfigurationProperties("github.client")
-    public AuthorizationCodeResourceDetails resourceDetails() {
-        return new AuthorizationCodeResourceDetails();
-    }
-
-    @Bean
-    @ConfigurationProperties("github.resource")
-    public ResourceServerProperties resource() {
-        return new ResourceServerProperties();
+    @ConfigurationProperties("github")
+    public MyResourcesWrapper github() {
+        return new MyResourcesWrapper();
     }
 
     private Filter ssoFilter() {
         OAuth2ClientAuthenticationProcessingFilter filter =
                 new OAuth2ClientAuthenticationProcessingFilter("/login/github");
-        OAuth2RestTemplate template = new OAuth2RestTemplate(resourceDetails(), oAuth2ClientContext);
+        MyResourcesWrapper wrapper = github();
+        OAuth2RestTemplate template = new OAuth2RestTemplate(wrapper.getClient(), oAuth2ClientContext);
         filter.setRestTemplate(template);
-        ResourceServerProperties resource = resource();
-        UserInfoTokenServices tokenServices = new UserInfoTokenServices(resource.getUserInfoUri(),
-                resource.getClientId());
+        UserInfoTokenServices tokenServices =
+                new UserInfoTokenServices(
+                        wrapper.getResource().getUserInfoUri(),
+                        wrapper.getResource().getClientId());
         tokenServices.setRestTemplate(template);
-        filter.setTokenServices(
-                new UserInfoTokenServices(resource.getUserInfoUri(), resource.getClientId()));
+        filter.setTokenServices(tokenServices);
         return filter;
+    }
+
+    @Configuration
+    @EnableResourceServer
+    protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            // @formatter:off
+			http.antMatcher("/me").authorizeRequests().anyRequest().authenticated();
+			// @formatter:on
+        }
+    }
+}
+
+class MyResourcesWrapper {
+
+    @NestedConfigurationProperty
+    private AuthorizationCodeResourceDetails client = new AuthorizationCodeResourceDetails();
+
+    @NestedConfigurationProperty
+    private ResourceServerProperties resource = new ResourceServerProperties();
+
+    public AuthorizationCodeResourceDetails getClient() {
+        return client;
+    }
+
+    public ResourceServerProperties getResource() {
+        return resource;
     }
 }
